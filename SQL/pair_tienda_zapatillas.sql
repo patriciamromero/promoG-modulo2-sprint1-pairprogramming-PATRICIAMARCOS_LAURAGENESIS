6303829a7e63d1fbefea5ec26d835bca13603830
Laura Genesis <lauragenesis@Laura.localdomain>
2024-02-23 12:01:53 +0100

    Pair_programming_3

--- SQL/pair_tienda_zapatillas.sql
+++ SQL/pair_tienda_zapatillas.sql
@@ -5,8 +5,8 @@ USE `tienda_zapatillas`;
 CREATE TABLE zapatillas( 
 	id_zapatilla  INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     modelo VARCHAR(45) NOT NULL,
-    color VARCHAR(45) NOT NULL
-);
+    color VARCHAR(45) NOT NULL,
+    );
 
 CREATE TABLE clientes( 
 	id_cliente INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
@@ -45,3 +45,42 @@ CREATE TABLE Facturas(
 		FOREIGN KEY (id_cliente) 
         REFERENCES clientes (id_cliente)
 );
+ALTER TABLE zapatillas ADD marca VARCHAR(45) NOT NULL;
+ALTER TABLE zapatillas ADD talla INT NOT NULL;
+
+ALTER TABLE Empleados 
+MODIFY COLUMN salario FLOAT;
+
+ALTER TABLE clientes DROP COLUMN pais;
+
+ALTER TABLE facturas ADD COLUMN total FLOAT;
+
+INSERT INTO zapatillas
+	VALUES (1,'XQYUN','Negro','Nike',42),
+			(2,'UOPMN','Rosas','Nike',39),
+			(3,'OPNYT','Verdes','Adidas',35);
+            
+INSERT INTO Empleados 
+    VALUES (1,'Laura','Alcobendas',25.987,'2010/09/03'),
+			(2,'Maria','Sevilla','0','2001/04/11' ),
+            (3,'Ester','Oviedo',30165.98,'2000/11/29');
+
+INSERT INTO clientes
+	VALUES (1,'Monica',1234567289,'monica@email.com', 'Calle Felicidad','Móstoles','Madrid',28176),
+			(2,'Lorena',289345678,'lorena@email.com','Calle Alegria','Barcelona','Barcelona',12346),	
+            (3,'Carmen',298463759,'carmen@email.com','Calle del Color','Vigo','Pontevedra',23456);
+
+INSERT INTO facturas
+	VALUES (1,123,'2001/12/11',1,2,1,54.98),
+			(2,1234,'2005/05/23',1,1,3,89.91),
+            (3,12345,'2015/09/18',2,3,3,76.23);
+            
+UPDATE zapatillas
+SET color = 'amarilla'WHERE color= 'rosas';
+
+
+UPDATE empleadas
+SET tienda = 'A Coruña' WHERE id_empleado= 1;
+
+
+
